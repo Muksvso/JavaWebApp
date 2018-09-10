ceeda80a99cfdad9516f9b87e8d9934dfb3bf07a
Muksvso <mukta.batra@live.com>
2018-09-10 14:55:53 -0700

    Update init.js

--- HorizonsGit/WebContent/js/init.js
+++ HorizonsGit/WebContent/js/init.js
@@ -1,7 +1,7 @@
 /*
 	Horizons by TEMPLATED
     templated.co @templatedco
-    Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
+    Released for free under the/ Creative Commons Attribution 3.0 license (templated.co/license)
 */
 (function($) {
 
